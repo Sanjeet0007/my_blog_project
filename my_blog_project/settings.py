9df370ef65f6e755c5a7475ec6b13b30550d5fd8
Sanjeet Kumar <162409264+Sanjeet0007@users.noreply.github.com>
2026-01-22 20:06:42 +0530

    Update settings.py

--- my_blog_project/settings.py
+++ my_blog_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h7a-8l$^1e49)6@96g=!y$6)(rnn3*ynx(rsxv&l&jha90-s&1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
